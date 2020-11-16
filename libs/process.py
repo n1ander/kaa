aed5dc284addbf0c1d77f9b5f8fb69b8ecb205a0
deckvrd <deckvrd@gmail.com>
2020-11-16 08:39:01 -0500

    docs: refactoring
    Remove unused functions in process.py.

--- libs/process.py
+++ libs/process.py
@@ -15,13 +15,3 @@ def getFileName(res):
         filename = os.path.basename(x)
         mylist.append(filename)
     return mylist
-
-def getHarmony(mylist):
-    for file in mylist:
-        if file.startswith('SHY'):
-            return file
-
-def getTaylorCreek(mylist):
-    for file in mylist:
-        if file.startswith('STC'):
-            return file
